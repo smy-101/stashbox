1388728b207e8114a6e5d0dc77b09fd5ce72c61e
smy-101 <69593167+smy-101@users.noreply.github.com>
2024-11-13 11:39:51 +0000

    成功设置容器信息

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "zany-goggles-vw5q6v7gp6qf6xx4-3000.app.github.dev"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
